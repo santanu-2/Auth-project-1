77864856d125849cd64de2779aabe9c67aa4f5e4
santanu chowdhury <schowdhu220@gmail.com>
2025-02-26 12:15:07 +0530

    URL update

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,6 +1,9 @@
 import { create } from "zustand";
 import axios from "axios";
-const BASE_URL = "http://localhost:5000/api/auth";
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000/api/auth"
+    : "/api/auth";
 axios.defaults.withCredentials = true;
 export const useAuthStore = create((set) => ({
   user: null,
